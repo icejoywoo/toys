d469f01cd530b662786cd4c676b62873a3a89831
icejoywoo <icejoywoo@gmail.com>
2015-05-10 23:15:21 +0800

    cubes does not support mongodb backend

---
+++ cubes/mongo_backend/import_data_to_mongo.py
@@ -0,0 +1,21 @@
+__author__ = 'icejoywoo'
+
+import pymongo
+
+mongo = pymongo.MongoClient(host="localhost", port=27017)
+
+query_result = mongo.test.query_result
+
+field_names = ['userid', 'count', 'channel', 'region', 'version']
+
+buffer = []
+
+for line in open("data.csv"):
+    item = dict(zip(field_names, line.rstrip('\n').split(',')))
+    buffer.append(item)
+    if len(buffer) >= 10000:
+        query_result.insert(buffer)
+        buffer = []
+
+if buffer:
+    query_result.insert(buffer)
