fe49213695b2c3d4ba9007df5df53e4e68a0544c
icejoywoo <icejoywoo@gmail.com>
2015-08-27 11:49:25 +0800

    add structlog demo

--- thirdparty/structlog_usage/hello.py
+++ thirdparty/structlog_usage/hello.py
@@ -24,16 +24,21 @@ logger.info('user.logged_in', happy=True, num_list=[1, 2, 3])
 
 
 # json log
+# print log to stdout stream
 logging.basicConfig(stream=sys.stdout, format='%(message)s')
+
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
 FORMAT = '%(asctime)s %(threadName)s %(lineno)d %(levelname)s %(message)s'
 log_dir = os.path.join(os.path.dirname(__file__), "log")
+
 if not os.path.isdir(log_dir):
     os.makedirs(log_dir)
+
 fa = logging.handlers.RotatingFileHandler(os.path.join(log_dir, "test.log"), 'a', 20*1024*1024, 2)
 fa.setLevel(logging.DEBUG)
 fa.setFormatter(logging.Formatter(FORMAT))
+# print log to log file
 _logger.addHandler(fa)
 
 logger = wrap_logger(
