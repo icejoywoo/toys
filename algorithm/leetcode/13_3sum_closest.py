d3656de236457d5542b47fa76568b8d1a9a71041
icejoywoo <icejoywoo@icejoywoodeMacBook-Pro.local>
2017-03-22 19:58:42 +0800

    update

--- algorithm/leetcode/13_3sum_closest.py
+++ algorithm/leetcode/13_3sum_closest.py
@@ -2,8 +2,17 @@
 # encoding: utf-8
 
 """
-    @brief: 
+    @brief: https://leetcode.com/problems/3sum-closest/?tab=Description
     @author: wujiabin@baidu.com
     @date: 07/03/2017
 """
 
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        result = None
