d879c4c18121802a573623886d520302ddce30b2
icejoywoo <icejoywoo@icejoywoodeMacBook-Pro.local>
2017-03-30 10:30:06 +0800

    update

--- machine_learning/06_svm/demo.py
+++ machine_learning/06_svm/demo.py
@@ -7,3 +7,31 @@
     @date: 27/03/2017
 """
 
+import time
+import numpy as np
+import svmMLiA
+
+dataAttr, labelAttr = svmMLiA.loadDataSet('testSet.txt')
+
+# 取值为 -1 和 1，与logistic回归不同
+print labelAttr
+
+start = time.time()
+b, alphas = svmMLiA.smoSimple(dataAttr, labelAttr, C=0.6, toler=0.001, maxIter=40)
+print "smoSimple elapsed: ", time.time() - start
+
+print b, alphas[alphas > 0]
+
+print np.shape(alphas[alphas > 0])
+
+for i in range(100):
+    if alphas[i] > 0:
+        print alphas[i], labelAttr[i]
+
+
+dataAttr, labelAttr = svmMLiA.loadDataSet('testSet.txt')
+
+start = time.time()
+b, alphas = svmMLiA.smoP(dataAttr, labelAttr, 0.6, 0.001, 40)
+print "smoP elapsed: ", time.time() - start
+
--- machine_learning/numpy_demo/demo.py
+++ machine_learning/numpy_demo/demo.py
@@ -10,4 +10,18 @@ import numpy as np
 # create a 3D numpy array
 arr = np.zeros((3, 3, 3))
 
-print arr
+
+a = np.array([
+    [11, 12, 13],
+    [21, 22, 23],
+    [31, 32, 33],
+])
+print a.T
+# 多维数据用 , 分割，表示对不同维度的操作
+print a[1,:]
+
+print np.nonzero(a[:,0])[0]
+
+print np.eye(5)
+
+print np.linalg.det(a)  # det == 0.0 说明矩阵不可逆
