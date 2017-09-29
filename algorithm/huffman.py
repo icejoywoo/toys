56875a4382f07ca3aa7fdce4ee9c79b7857f7a18
icejoywoo <icejoywoo@gmail.com>
2017-09-29 10:32:24 +0800

    update

---
+++ algorithm/huffman.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Le codage de Huffman
+# Théorie de l'information et du codage
+# Etudiant: Boubakr NOUR <n.boubakr@gmail.com>
+# Universite Djilali Liabes (UDL) de Sidi Bel Abbes
+# https://gist.github.com/nboubakr/0eec4ea650eeb6dc21f9
+
+import heapq
+from collections import defaultdict
+
+
+def encode(frequency):
+    heap = [[weight, [symbol, '']] for symbol, weight in frequency.items()]
+    heapq.heapify(heap)
+    while len(heap) > 1:
+        lo = heapq.heappop(heap)
+        hi = heapq.heappop(heap)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+
+data = "The frog at the bottom of the well drifts off into the great ocean"
+frequency = defaultdict(int)
+for symbol in data:
+    frequency[symbol] += 1
+
+huff = encode(frequency)
+print "Symbol".ljust(10) + "Weight".ljust(10) + "Huffman Code"
+for p in huff:
+    print p[0].ljust(10) + str(frequency[p[0]]).ljust(10) + p[1]
---
+++ crawler/webkit/demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+
+"""
+    @brief: 
+    @author: icejoywoo
+    @date: 16/05/2017
+"""
+
