9fb8cdf4319c5bfc0e6f308cbee5aea1ee55b5b6
icejoywoo <icejoywoo@gmail.com>
2018-10-17 09:54:48 +0800

    update

--- algorithm/leetcode/27_remove_elements.py
+++ algorithm/leetcode/27_remove_elements.py
@@ -4,31 +4,30 @@
 """
     @brief: 
     @author: icejoywoo
-    @date: 2018/10/16
+    @date: 2018/10/17
 """
 
 
 class Solution(object):
-    def removeDuplicates(self, nums):
+    def removeElement(self, nums, val):
         """
         :type nums: List[int]
+        :type val: int
         :rtype: int
         """
-        last = None
         i = 0
         l = len(nums)
         while i < l:
-            if nums[i] == last:
+            if nums[i] == val:
                 del nums[i]
                 l -= 1
             else:
-                last = nums[i]
                 i += 1
         return len(nums)
 
 
 if __name__ == '__main__':
     s = Solution()
-    assert s.removeDuplicates([1]) == 1
-    assert s.removeDuplicates([1, 2, 3]) == 3
-    assert s.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == 5
+    assert s.removeElement([1], 1) == 0
+    assert s.removeElement([1, 2, 3], 2) == 2
+    assert s.removeElement([0, 0, 1, 1, 1, 2, 2, 3, 3, 4], 1) == 7
