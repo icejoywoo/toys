add048a72f59c2a5bfd66ee766a892c0ece34c38
icejoywoo <icejoywoo@gmail.com>
2019-09-26 21:34:37 +0800

    leetcode 206

---
+++ algorithm/leetcode/206_reverse_linked_list.py
@@ -0,0 +1,58 @@
+# encoding: utf-8
+# https://leetcode.com/problems/reverse-linked-list/
+
+# helper function to convert python list to ListNode
+def list_node(l):
+    if l:
+        nodes = [ListNode(i) for i in l]
+        head = nodes[0]
+        p = head
+        for i in nodes[1:]:
+            p.next = i
+            p = i
+        return head
+    else:
+        return None
+
+# Definition for singly-linked list.
+class ListNode(object):
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def _to_list(self):
+        p = self
+        r = []
+        while p:
+            r.append(p.val)
+            p = p.next
+        return r
+
+    def __repr__(self):
+        return 'ListNode{val: %r, next: %r}' % (self.val, self.next._to_list() if self.next else None)
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # empty list
+        if head is None:
+            return head
+
+        p = None
+        q = head
+        while q:
+            t = q.next
+            q.next = p
+            p, q = q, t
+
+        return p
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.reverseList(list_node([1,2,3,4,5]))
