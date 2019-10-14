3ebcd53941c0859c9948945cd8f609d091118bf6
icejoywoo <icejoywoo@gmail.com>
2019-10-14 18:23:21 +0800

    leetcode 122

--- algorithm/leetcode/122_best-time-to-buy-and-sell-stock-ii.py
+++ algorithm/leetcode/122_best-time-to-buy-and-sell-stock-ii.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 """
-    @brief: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    @brief: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
     @author: icejoywoo
     @date: 2019-10-14
 """
@@ -15,22 +15,16 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+
         ret = 0
         buy = sys.maxint
-        buy_day = sell_day = None
-
         for i, e in enumerate(prices):
             if e > buy:
-                profit = e - buy
-                if profit > ret:
-                    sell_day = i + 1
-                    ret = profit
+                if e - buy > 0:
+                    ret += e - buy
+                    buy = e
             else:
                 buy = e
-                buy_day = i + 1
-
-        if buy_day and sell_day:
-            print buy_day, sell_day
 
         return ret
 
@@ -38,5 +32,6 @@ class Solution(object):
 if __name__ == '__main__':
     s = Solution()
     assert s.maxProfit([]) == 0
-    assert s.maxProfit([7, 1, 5, 3, 6, 4]) == 5
+    assert s.maxProfit([7, 1, 5, 3, 6, 4]) == 7
+    assert s.maxProfit([1, 2, 3, 4, 5]) == 4
     assert s.maxProfit([7, 6, 4, 3, 1]) == 0
