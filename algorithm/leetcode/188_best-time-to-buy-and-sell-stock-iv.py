3be42c7356adb1ac4b75a31cefb0ef5da77d06ca
icejoywoo <icejoywoo@gmail.com>
2019-10-15 01:00:39 +0800

    leetcode 188 update

--- algorithm/leetcode/188_best-time-to-buy-and-sell-stock-iv.py
+++ algorithm/leetcode/188_best-time-to-buy-and-sell-stock-iv.py
@@ -9,31 +9,8 @@
 
 
 class Solution(object):
-    def maxProfitDP(self, k, prices):
-        """
-        :type k: int
-        :type prices: List[int]
-        :rtype: int
-        """
-        if not prices:
-            return 0
-
-        l = len(prices)
-        dp = [[[0, 0] for _ in range(k+1)] for _ in range(l+1)]
-        dp[0] = [[0, -prices[0]] for _ in range(k+1)]
-        res = 0
-
-        for i in range(1, l):
-            for j in range(k):
-                dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i])
-                dp[i][j][1] = max(dp[i-1][j+1][0] - prices[i], dp[i-1][j][1])
-                res = max(res, dp[i][j][0], dp[i][j][1])
-
-        return res
-
     def maxProfit(self, k, prices):
         """
-        需要状态压缩，不知道如何压缩
         :type k: int
         :type prices: List[int]
         :rtype: int
@@ -42,24 +19,43 @@ class Solution(object):
             return 0
 
         l = len(prices)
-        dp = [[[0, 0] for _ in range(2)] for _ in range(2)]
-        dp[0] = [[0, -prices[0]] for _ in range(2)]
-        res = 0
+
+        # 交易次数超过数组的一半说明可以直接贪婪交易了
+        if k > l/2:
+            return self.maxProfitGreedy(prices)
+
+        dp = [[[0, 0] for _ in range(k+1)] for _ in range(2)]
+        dp[0] = [[0, -prices[0]] for _ in range(k+1)]
 
         for i in range(1, l):
             for j in range(k):
-                x, x0 = i % 2, (i-1) % 2
-                y, y1 = j % 2, (j+1) % 2
-                dp[x][y][0] = max(dp[x0][y][0], dp[x0][y][1] + prices[i])
-                dp[x][y][1] = max(dp[x0][y1][0] - prices[i], dp[x0][y][1])
-                res = max(res, dp[x][y][0], dp[x][y][1])
+                x, x0 = i % 2, (i - 1) % 2
+                dp[x][j][0] = max(dp[x0][j][0], dp[x0][j][1] + prices[i])
+                dp[x][j][1] = max(dp[x0][j+1][0] - prices[i], dp[x0][j][1])
+
+        res = [dp[(l-1) % 2][i][0] for i in range(k)]
+        if res:
+            return max(res)
+        else:
+            return 0
+
+    def maxProfitGreedy(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        res = 0
+        l = len(prices)
+        for i in range(1, l):
+            if prices[i] > prices[i-1]:
+                res += prices[i] - prices[i-1]
 
         return res
 
 
 if __name__ == '__main__':
     s = Solution()
-    print s.maxProfit(2, [3, 3, 5, 0, 0, 3, 1, 4])
     assert s.maxProfit(2, [2, 4, 1]) == 2
     assert s.maxProfit(2, [3, 2, 6, 5, 0, 3]) == 7
     assert s.maxProfit(2, [3, 3, 5, 0, 0, 3, 1, 4]) == 6
