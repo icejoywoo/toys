9e32907f750e56b419bb6735d285ce25fd4bf188
icejoywoo <icejoywoo@gmail.com>
2019-10-15 12:37:23 +0800

    leetcode 300

--- algorithm/leetcode/300_longest_increasing_subsequence.py
+++ algorithm/leetcode/300_longest_increasing_subsequence.py
@@ -8,7 +8,7 @@
 """
 
 # 暴力递归法，遍历所有的组合情况，然后算最大的，时间复杂度为2^n
-class Solution(object):
+class SolutionBF(object):
 
     result = set()
 
@@ -37,6 +37,27 @@ class Solution(object):
                 a = t
                 l = len(t)
         return l
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if not nums:
+            return 0
+
+        l = len(nums)
+        dp = [1 for _ in range(l)]
+
+        for i in range(l):
+            pre_dp = [dp[j] for j in range(i) if nums[i] > nums[j]]
+            max_pre_dp = max(pre_dp) if pre_dp else 0
+            dp[i] = max_pre_dp + 1
+
+        return max(dp)
 
 
 if __name__ == '__main__':
