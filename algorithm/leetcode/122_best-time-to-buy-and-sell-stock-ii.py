3903a399118a320f7c4b94540bb4965c7c156a42
icejoywoo <icejoywoo@gmail.com>
2019-10-15 10:07:30 +0800

    leetcode 122 add greedy method

--- algorithm/leetcode/122_best-time-to-buy-and-sell-stock-ii.py
+++ algorithm/leetcode/122_best-time-to-buy-and-sell-stock-ii.py
@@ -10,7 +10,7 @@ import sys
 
 
 class Solution(object):
-    def maxProfit(self, prices):
+    def maxProfit1(self, prices):
         """
         :type prices: List[int]
         :rtype: int
@@ -27,6 +27,21 @@ class Solution(object):
                 buy = e
 
         return ret
+
+    def maxProfit(self, prices):
+        """ 贪婪法
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+
+        res = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                res += prices[i] - prices[i-1]
+
+        return res
 
 
 if __name__ == '__main__':
